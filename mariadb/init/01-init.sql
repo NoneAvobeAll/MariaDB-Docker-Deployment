6adb00de58a5c36112c692ae06d1e7aeec92503b
Abubakkar Khan Fazla Rabbi <33498915+NoneAvobeAll@users.noreply.github.com>
2025-09-01 12:08:32 +0600

    Update 01-init.sql

---
+++ mariadb/init/01-init.sql
@@ -0,0 +1,10 @@
+-- USE backend;
+
+-- CREATE TABLE IF NOT EXISTS users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(50) NOT NULL UNIQUE,
+--     email VARCHAR(100) NOT NULL UNIQUE,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- no need to create 
